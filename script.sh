86806b0a6056b7b854578adc877fe5af599ea77e
Manziine <a.manziine@alustudent.com>
2025-02-11 12:18:43 +0200

    Add directories and counter script

--- script.sh
+++ script.sh
@@ -7,7 +7,9 @@ do
   mkdir -p "my_workspace/$i"
   touch "my_workspace/$i/.gitkeep"  
 done
-  
+
+ counter_file="my_workspace/counter_output.txt"
+ touch $counter_file
   for (( i =0 ; i <= 100 ; i++)) 
  do
   echo "counter: $i"
