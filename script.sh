06e61d34eafa3c247d6671985dbc932832f47130
Manziine <a.manziine@alustudent.com>
2025-02-11 09:53:23 +0200

    script for items

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Create the main directory
 mkdir -p "my_workspace"
-  for i in pen book key
-  do
- mkdir -p "my_workspace/$i"
- touch "my_workspace/$i"
+
+for i in pen book key
+do
+  mkdir -p "my_workspace/$i"
+  touch "my_workspace/$i/.gitkeep"  
+done
+
 
